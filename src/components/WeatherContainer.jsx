d479d18be012f214930756ae0398e5533b9e7254
Gian Pumayalla <gian.pumayalla@gmail.com>
2018-12-07 00:46:45 +0100

    creati componenti

---
+++ src/components/WeatherContainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Location from "./Location";
+import WeatherExtraInfo from "./WeatherExtraInfo";
+
+const WeatherContainer = () => (
+    <div>
+        <Location/>
+        <WeatherExtraInfo/>
+    </div>
+);
+export default WeatherContainer;
---
+++ src/components/WeatherTemperature.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const WeatherTemperature = () => (
+    <div>
+        WeatherTemperature
+    </div>
+);
+export default WeatherTemperature;
