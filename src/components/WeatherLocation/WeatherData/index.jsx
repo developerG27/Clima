c9c97e5305262b149099dd14872423c8c415bc89
Gian Pumayalla <gian.pumayalla@gmail.com>
2018-12-07 09:23:11 +0100

    Destructuring doppio

--- src/components/WeatherLocation/WeatherData/index.jsx
+++ src/components/WeatherLocation/WeatherData/index.jsx
@@ -2,22 +2,21 @@ import React from 'react';
 import WeatherExtraInfo from "./WeatherExtraInfo";
 import WeatherTemperature from "./WeatherTemperature";
 import {
-    CLOUD,
-    CLOUDY,
     SUN,
-    RAIN,
-    SNOW,
-    WINDY,
 } from './../../../constants/weather.js';
 import './styles.css';
 
-const Index = () => (
-    <div className="weatherDataCont">
-        <WeatherTemperature
-            temperature={20}
-            weatherState={SUN}
-        />
-        <WeatherExtraInfo humidity={80} wind={"10m/S"}/>
-    </div>
-);
-export default Index;
+const WeatherData = ({ data: { temperature, weatherState, humidity, wind} }) => {
+    return (
+        <div className="weatherDataCont">
+            <WeatherTemperature
+                temperature={temperature}
+                weatherState={weatherState}
+            />
+
+            <WeatherExtraInfo humidity={humidity} wind={wind}/>
+        </div>
+    );
+};
+
+export default WeatherData;
--- src/components/WeatherLocation/index.jsx
+++ src/components/WeatherLocation/index.jsx
@@ -2,11 +2,21 @@ import React from 'react';
 import WeatherData from "./WeatherData";
 import Location from './Location';
 import './styles.css';
+import {
+    SUN,
+} from './../../constants/weather.js';
+
+const data = {
+    temperature: 6,
+    weatherState: SUN,
+    humidity: 10,
+    wind: '10 m/s',
+}
 
 const WeatherContainer = () => (
     <div className="weatherLocationCont">
         <Location city="Buenos Aires"/>
-        <WeatherData/>
+        <WeatherData data={data}/>
     </div>
 );
 export default WeatherContainer;
