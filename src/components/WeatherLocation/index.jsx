23c236126eec61417faad1ace822b93b03e2721f
Gian Pumayalla <gian.pumayalla@gmail.com>
2018-12-07 11:02:06 +0100

    Class Component

--- src/components/WeatherLocation/index.jsx
+++ src/components/WeatherLocation/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import WeatherData from "./WeatherData";
 import Location from './Location';
 import './styles.css';
@@ -13,10 +13,30 @@ const data = {
     wind: '10 m/s',
 }
 
-const WeatherContainer = () => (
-    <div className="weatherLocationCont">
-        <Location city="Buenos Aires"/>
-        <WeatherData data={data}/>
-    </div>
-);
+
+class WeatherContainer extends Component {
+   constructor(){
+      super();
+      this.state = {
+         city: 'Buenos Aires',
+         data: data,
+      };
+   }
+
+   handleUpdateClick = () => {
+      console.log("actualizado");
+
+
+   }
+   render(){
+      const { data, city } = this.state;
+      return(
+         <div className="weatherLocationCont">
+            <Location city={city}/>
+            <WeatherData data={data}/>
+            <button onClick={this.handleUpdateClick}>Aggiornare</button>
+         </div>
+      );
+   }
+}
 export default WeatherContainer;
