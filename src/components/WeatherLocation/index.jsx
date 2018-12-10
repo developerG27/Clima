12686a0d64f48c75968fac7e860ce97c46dfb628
Gian Pumayalla <gian.pumayalla@gmail.com>
2018-12-10 22:26:45 +0100

    aggiunto conversione da gradi kelvin a celsius

--- src/components/WeatherLocation/index.jsx
+++ src/components/WeatherLocation/index.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import convert from 'convert-units';
 import WeatherData from "./WeatherData";
 import Location from './Location';
 import './styles.css';
@@ -19,7 +20,6 @@ const data = {
     wind: '10 m/s',
 }
 
-
 class WeatherContainer extends Component {
    constructor(){
       super();
@@ -29,8 +29,42 @@ class WeatherContainer extends Component {
       };
    }
 
+   getTemp = kelvin => {
+      return Number(convert(kelvin).from("K").to("C").toFixed(2));
+   }
+
+   getWeatherState = weather_data => {
+   return SUN;
+   }
+
+   getData = weather_data => {
+      const {humidity, temp} = weather_data.main;
+      const {speed} = weather_data.wind;
+      const weatherState = this.getWeatherState(weather_data);
+      const temperature = this.getTemp(temp);
+
+      const data = {
+         humidity,
+         temperature,
+         weatherState,
+         wind: `${speed} m/s`,
+      }
+      return data;
+   }
+
+
    handleUpdateClick = () => {
-      fetch(api_weather);
+      fetch(api_weather).then(resolve => {
+         console.log(resolve);
+         return resolve.json();
+      }).then(data =>{
+         const newWeather = this.getData(data);
+         console.log(newWeather);
+         debugger;
+         this.setState({
+            data: newWeather
+         });
+      });
    }
 
    render(){
