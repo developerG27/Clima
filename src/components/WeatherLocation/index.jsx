6b69290666216e70ae50410757ec1143f1a2ebdf
Gian Pumayalla <gian.pumayalla@gmail.com>
2018-12-10 22:46:22 +0100

    riordinato il codice

--- src/components/WeatherLocation/index.jsx
+++ src/components/WeatherLocation/index.jsx
@@ -1,17 +1,12 @@
 import React, { Component } from 'react';
-import convert from 'convert-units';
 import WeatherData from "./WeatherData";
 import Location from './Location';
 import './styles.css';
 import {
     SUN,
 } from './../../constants/weather.js';
-
-const location = 'Buenos Aires,ar';
-const api_key = '5adbdcb6d272223a6932ebb1d0918e82';
-const url_base_weather = "http://api.openweathermap.org/data/2.5/weather";
-const api_weather = `${url_base_weather}?q=${location}&appid=${api_key}`;
-
+import transformWeather from '../../services/transformWeather';
+import { api_weather } from '../../constants/api_url';
 
 const data = {
     temperature: 6,
@@ -29,36 +24,12 @@ class WeatherContainer extends Component {
       };
    }
 
-   getTemp = kelvin => {
-      return Number(convert(kelvin).from("K").to("C").toFixed(2));
-   }
-
-   getWeatherState = weather_data => {
-   return SUN;
-   }
-
-   getData = weather_data => {
-      const {humidity, temp} = weather_data.main;
-      const {speed} = weather_data.wind;
-      const weatherState = this.getWeatherState(weather_data);
-      const temperature = this.getTemp(temp);
-
-      const data = {
-         humidity,
-         temperature,
-         weatherState,
-         wind: `${speed} m/s`,
-      }
-      return data;
-   }
-
-
    handleUpdateClick = () => {
       fetch(api_weather).then(resolve => {
          console.log(resolve);
          return resolve.json();
       }).then(data =>{
-         const newWeather = this.getData(data);
+         const newWeather = transformWeather(data);
          console.log(newWeather);
          debugger;
          this.setState({
