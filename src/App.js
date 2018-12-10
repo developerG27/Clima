afffa489b82b778633a66b1b893d91ec37ba7656
Gian Pumayalla <gian.pumayalla@gmail.com>
2018-12-11 00:27:09 +0100

    Array cities

--- src/App.js
+++ src/App.js
@@ -2,11 +2,19 @@ import React, { Component } from 'react';
 import './App.css';
 import LocationList from "./components/LocationList";
 
+const cities = [
+   "Buenos Aires,ar",
+   "Washington,us",
+   "Bogota,col",
+   "Ciudad de México,mx",
+   "Madrid,es",
+   "Lima,pe",
+];
 class App extends Component {
   render() {
     return (
        <div>
-         <LocationList/>
+         <LocationList cities={cities}/>
        </div>
     );
   }
--- src/components/LocationList.js
+++ src/components/LocationList.js
@@ -1,24 +1,32 @@
 import React from 'react';
 import WeatherContainer from "./WeatherLocation";
+import PropTypes from 'prop-types';
 
-const LocationList = () => (
-   <div>
-      <WeatherContainer
-         city="Buenos Aires,ar"
-      />
-      <WeatherContainer
-         city="Milano"
-      />
-      <WeatherContainer
-         city="Cina"
-      />
-      <WeatherContainer
-         city="Barcelona,es"
-      />
-      <WeatherContainer
-         city="Washington,US"
-      />
-   </div>
-)
+const LocationList = ({cities}) => {
+      return(
+         <div>
+               <WeatherContainer
+                  city="Buenos Aires,ar"
+               />
+               <WeatherContainer
+                  city="Milan,it"
+               />
+               <WeatherContainer
+                  city="Barcelona,es"
+               />
+               <WeatherContainer
+                  city="Washington,US"
+               />
+         </div>
+      )
+
+
+}
+
+
+
+LocationList.propTypes = {
+      cities: PropTypes.array.isRequired,
+};
 
 export default LocationList;
