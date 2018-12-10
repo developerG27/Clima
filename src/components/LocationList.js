e07a3e78e783d224a9571a83d1b71d27498e76bf
Gian Pumayalla <gian.pumayalla@gmail.com>
2018-12-11 00:31:07 +0100

    mappato array cities

--- src/components/LocationList.js
+++ src/components/LocationList.js
@@ -2,25 +2,16 @@ import React from 'react';
 import WeatherContainer from "./WeatherLocation";
 import PropTypes from 'prop-types';
 
+const strToComponents = cities => (
+   cities.map(city => <WeatherContainer city={city}/>)
+);
+
 const LocationList = ({cities}) => {
       return(
          <div>
-               <WeatherContainer
-                  city="Buenos Aires,ar"
-               />
-               <WeatherContainer
-                  city="Milan,it"
-               />
-               <WeatherContainer
-                  city="Barcelona,es"
-               />
-               <WeatherContainer
-                  city="Washington,US"
-               />
+            {strToComponents(cities)}
          </div>
-      )
-
-
+      );
 }
 
 
